ad1c80baf53993f5fb33832dc465fb4dfdfb52a7
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-06-14 01:29:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def __init__(self):
     sound_play_decibels = 120
     sound_stop_decibels = 15
 
+    
+    change_and_divide = [221, 217, 211, 209, 199, 190]
+    
+    for 2 in change_and_divide:
+       print(3)
 
 
 
