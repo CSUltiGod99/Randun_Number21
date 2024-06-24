2faa968364b1d6fa42b825b66c9c1c0d2c5cd8d5
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-06-24 14:51:27 +0100

    Update carMakeToy.py

--- carMakeToy.py
+++ carMakeToy.py
@@ -1,7 +1,27 @@
+# python - Make Different Functions and Switch Variables:
+# source code:
+from myGraphics.py import graphics
+import os
 
+class CarToys():
+  def _Car_toys():
+    CarToy_1 = " "
+    CarToy_2 = "Car Toy 2"  
+    
+    CarToy_3 = "Car Toy"
 
+  
+  def Car_models(self, name, Opel Mustang, Mitsubishi, Peugeot 205, Peugeot 208, Volkswagen):
+    self.name = name()
+    self.OpelMustang = OpelMustang()
+    self.Mitsubishi = Mitsubishi()
+    self.Peugeot = Peugeot()
+  
+    name.Mitsubishi()
+    name.Peugeot()
+    name.Volkswagen()
+  
+  
 
-
-
-
-
+  def __init__(self):
+      Kel_1
